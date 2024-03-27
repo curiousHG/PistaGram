82b604317c79967a492c9e3d2dc8776a14e05221
access-denied-001 <devang2000singh@gmail.com>
2024-03-27 11:42:46 +0530

    CREATE: created JWT tokens using user Id

---
+++ Backend/Utils/generateJWT.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generateJWT = (userId, res) => {
+    const webToken = jwt.sign({userId}, process.env.JWT_SECRET_KEY, {
+        expiresIn: "5d"
+    });
+
+    res.cookie("jwt", webToken,  {
+        maxAge: 5*24*60*60*1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.ENVIRONMENT !== "development"
+    });
+};
+
+export default generateJWT;
