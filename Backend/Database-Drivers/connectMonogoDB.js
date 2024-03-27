6661309511bb1732156d0379402a8b5633379693
access-denied-001 <devang2000singh@gmail.com>
2024-03-27 11:41:59 +0530

    CREATE: add monogo db to the project:

---
+++ Backend/Database-Drivers/connectMonogoDB.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+
+const connectMongoDB = async () => {
+    try {
+        await mongoose.connect(MONGO_DB_URI);
+        console.log(`Connection Successfull: Handshake with MONGO DB executed succesfully!`);
+    } catch (error) {
+        console.log("Connection Unsuccessfull: Handshake with MONGO DB failed!", error.message);
+    }
+};
+
+export default connectMongoDB;
