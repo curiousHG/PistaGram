23dccdaafa26023f614333f90456454314a2b54a
access-denied-001 <devang2000singh@gmail.com>
2024-03-27 11:42:19 +0530

    CREATE: created user model

---
+++ Backend/Models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    }, 
+    profilePicture: {
+        type: String,
+        default: "",
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
