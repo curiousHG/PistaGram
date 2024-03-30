fdf64bc49e4c8db10b14cbaede0179f2340e6203
access-denied-001 <devang2000singh@gmail.com>
2024-03-30 19:06:49 +0530

    CREATE: Configured tailwind css and daisy UI and made signup and login form

---
+++ Frontend/src/Pages/Login.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='h-full w-full bg-white-500 p-6 shadow-md rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border-4 border-gray-600'>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                    Login
+                </h1>
+                <form>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>
+                                Username
+                            </span>
+                        </label>
+                        <input type='text' className='w-full input input-bordered h-10'/>
+                    </div>                    
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>
+                                Password
+                            </span>
+                        </label>
+                        <input type='password' className='w-full input input-bordered h-10'/>
+                    </div>
+                    <a href="#" className='text-sm hover:underline hover:text-gray-100 mt-2 inline-block'>
+                        {"Don't"} have an account yet!
+                    </a>
+                    <div className='flex justify-center'>
+                        <button className="btn glass btn-md mt-2 mr-auto ml-auto">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
