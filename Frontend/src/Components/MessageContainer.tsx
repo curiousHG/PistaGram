7266b0d0647775b860f35801cec6af9d2bd27881
access-denied-001 <devang2000singh@gmail.com>
2024-04-01 19:22:55 +0530

    CREATE: create default chat section

--- Frontend/src/Components/MessageContainer.tsx
+++ Frontend/src/Components/MessageContainer.tsx
@@ -1,18 +1,43 @@
 import ReceiverInfo from "./ReceiverInfo.js";
 import MessageBody from "./MessageBody.js";
 import MessageInput from "./MessageInput.js";
+import { TiMessages } from "react-icons/ti";
 
-const MessageContainer = () => {
-    return (
-        <div className="flex flex-col justify-between bg-white-400 m-3 w-full rounded-3xl">
-            <ReceiverInfo
-                username="John Doe"
-                avatarUrl="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
-            <MessageBody />
-            <MessageInput />
-        </div>
-    );
+interface MessageContainerProps {
+    defaultView: boolean;
+}
+
+const MessageContainer = ({ defaultView }: MessageContainerProps) => {
+    const DefaultViewJSX = () => {
+        return (
+            <div className="flex items-center justify-center w-full h-full">
+                <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+                    <p> Welcome John Doe</p>
+                    <p> Select a room to view chat history</p>
+                    <TiMessages className="text-3xl" />
+                </div>
+            </div>
+        );
+    };
+
+    const RoomViewJSX = () => {
+        return (
+            <div className="flex flex-col justify-between bg-white-400 m-3 w-full rounded-3xl">
+                <ReceiverInfo
+                    username="John Doe"
+                    avatarUrl="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+                <MessageBody />
+                <MessageInput />
+            </div>
+        );
+    };
+
+    if (defaultView) {
+        return <DefaultViewJSX />;
+    } else {
+        return <RoomViewJSX />;
+    }
 };
 
 export default MessageContainer;
--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Sidebar from "../Components/Sidebar";
 import MessageContainer from "../Components/MessageContainer";
 
@@ -6,7 +5,7 @@ const Home = () => {
     return (
         <div className="flex h-4/5 w-3/5 rounded-xl overflow-hidden bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 p-3">
             <Sidebar />
-            <MessageContainer />
+            <MessageContainer defaultView={true} />
         </div>
     );
 };
