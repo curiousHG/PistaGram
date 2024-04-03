907d2148f23807afde13652f34160791ec0b6881
access-denied-001 <devang2000singh@gmail.com>
2024-04-03 22:35:21 +0530

    FIX: Fixed Romm and Message models

--- Backend/Models/Message.js
+++ Backend/Models/Message.js
@@ -1,29 +1,25 @@
 import mongoose, { Schema, model } from "mongoose";
 
-const schemaOptions = {
-    timestamps: {
-        createdAt: "created_at",
-        updatedAt: "updated_at",
+const messageSchema = new mongoose.Schema(
+    {
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        receiverId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
     },
-};
+    { timestamps: true }
+);
 
-const messageSchema = new mongoose.Schema({
-    senderId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    message: {
-        type: String,
-        required: true,
-    },
-});
-
-const Message = new model("Message", messageSchema, schemaOptions);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
--- Backend/Models/Room.js
+++ Backend/Models/Room.js
@@ -1,28 +1,24 @@
 import mongoose from "mongoose";
 
-const schemaOptions = {
-    timestamps: {
-        createdAt: "created_at",
-        updatedAt: "updated_at",
+const roomSchema = new mongoose.Schema(
+    {
+        people: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+        messages: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Message",
+                default: [],
+            },
+        ],
     },
-};
+    { timestamps: true }
+);
 
-const roomSchema = new mongoose.Schema({
-    people: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-    ],
-    messages: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Message",
-            default: [],
-        },
-    ],
-});
-
-const Room = mongoose.model("Room", roomSchema, schemaOptions);
+const Room = mongoose.model("Room", roomSchema);
 
 export default Room;
