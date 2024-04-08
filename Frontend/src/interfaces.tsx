f48b75b353d6007685bd4e91bd069ac37168a9cf
access-denied-001 <devang2000singh@gmail.com>
2024-04-08 23:38:22 +0530

    CREATE: created interface for the project

---
+++ Frontend/src/interfaces.tsx
@@ -0,0 +1,54 @@
+export interface IUser {
+    _id: string;
+    firstname: string;
+    lastname: string;
+    username: string;
+    profilePicture: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface IMessage {
+    _id: string;
+    senderId: string;
+    receiverId: string;
+    message: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface IRoom {
+    _id: string;
+    people: string[];
+    messages: string[];
+    createdAt: string;
+    updatedAt: string[];
+}
+
+export interface IMessageProps {
+    sender: IUser;
+    receiver: IUser;
+    message: IMessage;
+}
+
+export interface UserSidebarInfo {
+    room: IUser;
+    lastIndex: boolean;
+}
+
+export interface ILogin {
+    username: string;
+    password: string;
+}
+
+export interface SendMessageProps {
+    message: string;
+}
+
+export interface ISignup {
+    firstname: string;
+    lastname: string;
+    username: string;
+    email: string;
+    password: string;
+}
