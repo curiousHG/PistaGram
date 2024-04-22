d3408ad976c7b18db7b7bf031f93bd305821605d
access-denied-001 <devang2000singh@gmail.com>
2024-04-22 21:43:51 +0530

    CREATE: using tailwind css media queries add responsive design

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import MessageContainer from "../Components/MessageContainer";
 
 const Home = () => {
     return (
-        <div className="flex h-4/5 w-3/5 rounded-xl overflow-hidden bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 p-3">
+        <div className="flex my-3 max-[640px]:h-full max-[640px]:w-full sm:h-full sm:w-full md:h-4/5 md:w-full lg:h-4/5 lg:w-4/5 xl:h-4/5 xl:w-4/5 2xl:h-4/5 2xl:w-3/5 rounded-xl overflow-auto bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
             <Sidebar />
             <MessageContainer />
         </div>
