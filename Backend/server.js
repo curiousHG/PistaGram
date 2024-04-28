f0fd63d33af31743488ccd2b11430878df51b1c3
access-denied-001 <devang2000singh@gmail.com>
2024-04-28 19:25:05 +0530

    Deploy

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/messages", messagingRouter);
 
 app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
