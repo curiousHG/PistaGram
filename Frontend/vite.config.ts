65ab98c3086e1f8c3f8120757053b43dbb6eda9b
access-denied-001 <devang2000singh@gmail.com>
2024-04-28 19:56:03 +0530

    DEPLOY: removing config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    copy({
-      targets: [
-        { src: 'src/Assets', dest: 'dist' },
-      ]
-    }),
   ],
-  build: {
-    emptyOutDir: false,
-    outDir: 'dist',
-    assetsDir: 'Assets',
-  },
   server: {
     port: 5000,
     proxy: {
