b9ccdb5e1bc94453abcc3f13a77247782a9e84f8
access-denied-001 <devang2000singh@gmail.com>
2024-05-04 23:51:12 +0530

    CREATE: created the user info section with react-icons and using telegram UI

--- Frontend/src/Components/RoomInfo.tsx
+++ Frontend/src/Components/RoomInfo.tsx
@@ -3,6 +3,9 @@ import { useRoomContext } from "../Context/RoomContext";
 import { useUserInfoContext } from "../Context/UserInfoPopupContext";
 import { convertCreatedAt } from "../utils";
 import { CiSquareRemove } from "react-icons/ci";
+import { MdOutlineInfo } from "react-icons/md";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { BsFillCalendarDateFill } from "react-icons/bs";
 
 const RoomInfo = () => {
     const { selectedRoom } = useRoomContext();
@@ -11,39 +14,56 @@ const RoomInfo = () => {
     else {
         return (
             <div className="absolute inset-0 z-50 flex items-center justify-center backdrop-filter backdrop-blur-2xl">
-                <div className="h-full max-h-[500px] w-full max-w-[500px] bg-gray-800 rounded-2xl p-10 flex justify-end">
-                    <div className="mt-40 text-xl font-bold">
-                        <h2 className="text-3xl font-bolder mb-4">
-                            User Information
-                        </h2>
-                        <p>
-                            <span className="font-semibold">Username: </span>{" "}
-                            {selectedRoom.username}
-                        </p>
-                        <p>
-                            <span className="font-semibold">Firstname: </span>{" "}
-                            {selectedRoom.firstname}
-                        </p>
-                        <p>
-                            <span className="font-semibold">Lastname: </span>{" "}
-                            {selectedRoom.lastname}
-                        </p>
-                        <p>
-                            <span className="font-semibold">Email: </span>{" "}
-                            {selectedRoom.email}
-                        </p>
-                        <p>
-                            <span className="font-semibold">Created on: </span>{" "}
-                            {convertCreatedAt(selectedRoom.createdAt)}
+                <div className="h-full max-h-[500px] w-full max-w-[500px] bg-gray-800 rounded-2xl p-5 flex flex-col gap-4">
+                    <div className="text-xl font-bold w-full flex flex-col gap-4 items-center py-2 px-5">
+                        <div className="w-full flex justify-between">
+                            <p className="text-3xl text-bolder">User Info</p>
+                            <div
+                                className="pl-8 text-bolder cursor-pointer w-[50px]"
+                                onClick={() => {
+                                    setUserInfoPopup(false);
+                                }}
+                            >
+                                <CiSquareRemove size={40} />
+                            </div>
+                        </div>
+                        <div className="w-full flex gap-4 items-center">
+                            <img
+                                className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 cursor-pointer"
+                                src={selectedRoom.profilePicture}
+                                alt="Avatar for user"
+                            />
+                            <p className="text-xl text-bolder">
+                                {selectedRoom.firstname} {selectedRoom.lastname}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex gap-5 py-2 px-5">
+                        <MdOutlineInfo size={60} className="cursor-pointer" />
+                        <p className="text-xl text-bold">
+                            <p> {selectedRoom.username} </p>
+                            <p className="text-lg text-bolder"> User ID </p>
                         </p>
                     </div>
-                    <div
-                        className="p-5 text-bolder cursor-pointer"
-                        onClick={() => {
-                            setUserInfoPopup(false);
-                        }}
-                    >
-                        <CiSquareRemove size={45} />
+                    <div className="flex gap-5 py-2 px-5">
+                        <MdOutlineAlternateEmail
+                            size={60}
+                            className="cursor-pointer"
+                        />
+                        <p className="text-xl text-bold">
+                            <p> {selectedRoom.email} </p>
+                            <p className="text-lg text-bolder"> Email </p>
+                        </p>
+                    </div>
+                    <div className="flex gap-5 py-2 px-5">
+                        <BsFillCalendarDateFill
+                            size={60}
+                            className="cursor-pointer"
+                        />
+                        <p className="text-xl text-bold">
+                            <p> {convertCreatedAt(selectedRoom.createdAt)} </p>
+                            <p className="text-lg text-bolder"> Created On </p>
+                        </p>
                     </div>
                 </div>
             </div>
