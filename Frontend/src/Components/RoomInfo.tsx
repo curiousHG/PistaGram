d4c4b7aeec90190e72ef2e5c5e6e8cc3fe974232
access-denied-001 <devang2000singh@gmail.com>
2024-05-05 13:53:06 +0530

    STYLE: Minor text fix

--- Frontend/src/Components/RoomInfo.tsx
+++ Frontend/src/Components/RoomInfo.tsx
@@ -15,16 +15,16 @@ const RoomInfo = () => {
         return (
             <div className="absolute inset-0 z-50 flex items-center justify-center backdrop-filter backdrop-blur-2xl">
                 <div className="h-full max-h-[500px] w-full max-w-[500px] bg-gray-800 rounded-2xl p-5 flex flex-col gap-4">
-                    <div className="text-xl font-bold w-full flex flex-col gap-4 items-center py-2 px-5">
+                    <div className="text-lg font-bold w-full flex flex-col gap-4 items-center py-2 px-5">
                         <div className="w-full flex justify-between">
-                            <p className="text-3xl text-bolder">User Info</p>
+                            <p className="text-2xl text-bolder">User Info</p>
                             <div
                                 className="pl-8 text-bolder cursor-pointer w-[50px]"
                                 onClick={() => {
                                     setUserInfoPopup(false);
                                 }}
                             >
-                                <CiSquareRemove size={40} />
+                                <CiSquareRemove size={50} />
                             </div>
                         </div>
                         <div className="w-full flex gap-4 items-center">
@@ -33,34 +33,34 @@ const RoomInfo = () => {
                                 src={selectedRoom.profilePicture}
                                 alt="Avatar for user"
                             />
-                            <p className="text-xl text-bolder">
+                            <p className="text-bolder">
                                 {selectedRoom.firstname} {selectedRoom.lastname}
                             </p>
                         </div>
                     </div>
                     <div className="flex gap-5 py-2 px-5">
-                        <MdOutlineInfo size={60} className="cursor-pointer" />
-                        <div className="text-xl text-bold">
+                        <MdOutlineInfo size={50} className="cursor-pointer" />
+                        <div className="text-lg text-bold">
                             <p> {selectedRoom.username} </p>
                             <p className="text-lg text-bolder"> User ID </p>
                         </div>
                     </div>
                     <div className="flex gap-5 py-2 px-5">
                         <MdOutlineAlternateEmail
-                            size={60}
+                            size={50}
                             className="cursor-pointer"
                         />
-                        <div className="text-xl text-bold">
+                        <div className="text-lg text-bold">
                             <p> {selectedRoom.email} </p>
                             <p className="text-lg text-bolder"> Email </p>
                         </div>
                     </div>
                     <div className="flex gap-5 py-2 px-5">
                         <BsFillCalendarDateFill
-                            size={60}
+                            size={50}
                             className="cursor-pointer"
                         />
-                        <div className="text-xl text-bold">
+                        <div className="text-lg text-bold">
                             <p> {convertCreatedAt(selectedRoom.createdAt)} </p>
                             <p className="text-lg text-bolder"> Created On </p>
                         </div>
