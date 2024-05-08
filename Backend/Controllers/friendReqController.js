5fe89e85b3e46805ee0d1aa04b4c1e62816ef92f
access-denied-001 <devang2000singh@gmail.com>
2024-05-08 23:45:09 +0530

    CREATE: created endpoint that deletes req

--- Backend/Controllers/friendReqController.js
+++ Backend/Controllers/friendReqController.js
@@ -21,7 +21,46 @@ export const sendFriendReq = async (req, res) => {
 };
 
 export const deleteFriendReq = async (req, res) => {
-    try { 
+    try {
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        const sender = await User.findOne({ _id: senderId });
+        const receiver = await User.findOne({ _id: receiverId });
 
+        const senderOutgoingFriendsReq = sender.outgoingFriendsReq;
+        const receiverIncomingFriendReq = receiver.incomingFriendsReq;
+
+        const receiversIdFromSender = senderOutgoingFriendsReq.find(
+            (id) => id === receiverId
+        );
+
+        const senderIdFromReceiver = receiverIncomingFriendReq.find(
+            (id) => id === senderId
+        );
+
+        if (!receiversIdFromSender || !senderIdFromReceiver) {
+            throw new Error(
+                "Inconsistencies found for this request.. please try again"
+            );
+        }
+
+        sender.outgoingFriendsReq = senderOutgoingFriendsReq.filter(
+            (id) => id !== receiverId
+        );
+        receiver.incomingFriendsReq = receiverIncomingFriendReq.filter(
+            (id) => id !== senderId
+        );
+
+        await Promise.all([sender.save(), receiver.save()]);
+
+        return res.status(200);
+    } catch (error) {
+        console.log(
+            "Error in Delete Friend Request Controller: ",
+            error.message
+        );
+        res.status(500).json({
+            error: "Server Error: Internal error occurred during deleting friend req!",
+        });
     }
 };
--- Backend/Routers/friendsRouter.js
+++ Backend/Routers/friendsRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
 import secureRoute from "../Middleware/secureRoute.js";
-import { sendFriendReq } from "../Controllers/friendReqController.js";
+import {
+    deleteFriendReq,
+    sendFriendReq,
+} from "../Controllers/friendReqController.js";
 
 const friendsRouter = express.Router();
 
 friendsRouter.post("/sendReq/:id", secureRoute, sendFriendReq);
+friendsRouter.post("/deleteReq/:id", secureRoute, deleteFriendReq);
 
 export default friendsRouter;
