b70d29f5ff0de65fbe54c5f6430322ffc447b61e
access-denied-001 <devang2000singh@gmail.com>
2024-05-09 23:51:14 +0530

    REFACTOR

--- Backend/Controllers/Message.js
+++ Backend/Controllers/Message.js
@@ -2,30 +2,7 @@ import Message from "../Models/Message.js";
 import Room from "../Models/Room.js";
 import { getReceiverSocketId, io } from "../socket.js";
 
-export const getMessages = async (req, res) => {
-    try {
-        const { id: receiverId } = req.params;
-        const senderId = req.user._id;
-
-        const room = await Room.findOne({
-            people: { $all: [senderId, receiverId] },
-        }).populate("messages");
-
-        if (!room) {
-            return res.status(200).json([]);
-        }
-        const messages = room.messages;
-
-        return res.status(200).json(messages);
-    } catch (error) {
-        console.log("Error in Get Message Controller: ", error.message);
-        res.status(500).json({
-            error: "Server Error: Internal error occurred during message gathering!",
-        });
-    }
-};
-
-export const registerMessage = async (req, res) => {
+export const addMessage = async (req, res) => {
     try {
         const { message } = req.body;
         const { id: receiverId } = req.params;
@@ -68,6 +45,29 @@ export const registerMessage = async (req, res) => {
         console.log("Error in Register Message Controller: ", error.message);
         res.status(500).json({
             error: "Server Error: Internal error occurred during message registering!",
+        });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        const room = await Room.findOne({
+            people: { $all: [senderId, receiverId] },
+        }).populate("messages");
+
+        if (!room) {
+            return res.status(200).json([]);
+        }
+        const messages = room.messages;
+
+        return res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in Get Message Controller: ", error.message);
+        res.status(500).json({
+            error: "Server Error: Internal error occurred during message gathering!",
         });
     }
 };
--- Backend/Routers/Message.js
+++ Backend/Routers/Message.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getMessages, registerMessage } from "../Controllers/Message.js";
+import { getMessages, addMessage } from "../Controllers/Message.js";
 import secureRoute from "../Middleware/secureRoute.js";
 
 const messagingRouter = express.Router();
 
 messagingRouter.get("/:id", secureRoute, getMessages);
-messagingRouter.post("/send/:id", secureRoute, registerMessage);
+messagingRouter.post("/send/:id", secureRoute, addMessage);
 
 export default messagingRouter;
