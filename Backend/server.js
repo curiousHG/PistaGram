900232582f309e22a3f0b30427d8ae48519b7bab
access-denied-001 <devang2000singh@gmail.com>
2024-05-18 16:52:43 +0530

    UPDATE: Updated to SSR(server side rendering)

--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,17 @@ import userRouter from "./Routers/User.js";
 import { app, server } from "./socket.js";
 import requestRouter from "./Routers/Request.js";
 import friendsRouter from "./Routers/Friends.js";
+import path from "path";
 
 // Server Configs
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "Frontend", "dist")));
 
 app.get("/", (req, res) => {
     res.send("Server is runnning");
@@ -25,6 +30,10 @@ app.use("/api/users", userRouter);
 app.use("/api/messages", messagingRouter);
 app.use("/api/request", requestRouter);
 app.use("/api/friends", friendsRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 // Start Server on default PORT
 server.listen(PORT, async () => {
