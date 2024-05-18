19c4d4a4b30cd3b047c83cc95bfc2b30fce939ff
access-denied-001 <devang2000singh@gmail.com>
2024-05-18 16:18:08 +0530

    CREATE: Created info label for user information

--- Frontend/src/Components/MessageBody.tsx
+++ Frontend/src/Components/MessageBody.tsx
@@ -11,7 +11,7 @@ import useMessage from "../Hooks/useMessage";
 
 const MessageBody = () => {
     const { authUser } = useAuthContext();
-    const { selectedRoom } = useRoomContext();
+    const { selectedRoom, category } = useRoomContext();
     const { socket } = useSocketContext();
 
     // Send message hook
@@ -57,6 +57,10 @@ const MessageBody = () => {
         [selectedRoom._id]
     );
 
+    const checkDisabledStatus = (category: string) => {
+        return category === "friends" ? false : true;
+    };
+
     // Load messages in the room when selectedRoom is changed
     useEffect(() => {
         getMessages();
@@ -92,7 +96,7 @@ const MessageBody = () => {
             >
                 {loading ? (
                     <span className="loading loading-spinner"></span>
-                ) : (
+                ) : category === "friends" ? (
                     messages?.map((message) => (
                         <div key={message._id} ref={lastMessage}>
                             <Message
@@ -103,6 +107,24 @@ const MessageBody = () => {
                             />
                         </div>
                     ))
+                ) : (
+                    <div
+                        className="flex items-center bg-blue-600 bg-opacity-70 text-white text-md font-bolder px-4 py-3 rounded-lg"
+                        role="alert"
+                    >
+                        <svg
+                            className="fill-white w-10 h-9 mr-2 rounded-full p-2 bg-gray-800"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+                        </svg>
+                        <p>
+                            {selectedRoom.status === "pending"
+                                ? `Please accept the friend request from ${selectedRoom.username} to start a conversation!`
+                                : `Please send a friend request to ${selectedRoom.username} and wait for approval to start a conversation!`}
+                        </p>
+                    </div>
                 )}
             </div>
             <div className="mb-3 w-full flex justify-center items-center">
@@ -112,7 +134,7 @@ const MessageBody = () => {
                         className="grow"
                         placeholder="Search"
                         value={message}
-                        disabled={loading}
+                        disabled={loading || checkDisabledStatus(category)}
                         onChange={(e: any) => {
                             setMessage(e.target.value);
                         }}
