d4a280be0cc5ac878ea2108ddb0068ffd79c3faf
Devang Singh <devang2000singh@gmail.com>
2024-05-25 02:46:03 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5000,
     proxy: {
       "/api": {
-        target: "http://172.31.36.142/",
+        target: "http://localhost:8000/",
       }
     }
   }
