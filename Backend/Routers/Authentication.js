49a8d113105aed2476334fd70c0625164a6cccd4
access-denied-001 <devang2000singh@gmail.com>
2024-05-27 23:45:20 +0530

    CREATE: update router with debug level for loki

--- Backend/Routers/Authentication.js
+++ Backend/Routers/Authentication.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { login, logout, signup } from "../Controllers/Authentication.js";
+import { LOGGER } from "../server.js";
 
 const authenticationRouter = express.Router();
+
+LOGGER.debug(
+    `Authentication router - Request received on authentication router, sending it to respective controller`
+);
 
 authenticationRouter.post("/signup", signup);
 authenticationRouter.post("/login", login);
--- Backend/Routers/User.js
+++ Backend/Routers/User.js
@@ -6,8 +6,13 @@ import {
     getAllUsers,
     getPendingReqUsers,
 } from "../Controllers/User.js";
+import { LOGGER } from "../server.js";
 
 const userRouter = express.Router();
+
+LOGGER.debug(
+    `User router - Request received on user router, sending it to respective controller`
+);
 
 userRouter.get("/all", secureRoute, getAllUsers);
 userRouter.get("/friends", secureRoute, getAllFriends);
